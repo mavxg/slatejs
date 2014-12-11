00c6cbd41cf6e6bc7676a65afc0e8120702965d5
Benjamin Norrington <ben@norrington.net>
2014-12-11 09:33:43 +0000

    fixed operation transform containing removes and undo

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var assert = chai.assert;
 
 var Operations = require('../lib/operations');
 var m = require('../lib/model');
+var UndoManager = require('../lib/undo');
 
 describe('Operations', function() {
 });
@@ -17,7 +18,7 @@ describe('Apply', function() {
 		.insert({_type:'Section'})
 		.insert('this is more text')
 		.insert({_type:'H3'})
-		.retain(doc.length - 20);
+		.end(doc.length);
 
 	it('Can insert text into string', function() {
 		var ops = new Operations()
@@ -40,3 +41,87 @@ describe('Apply', function() {
 		assert.equal(JSON.stringify(y),JSON.stringify(doc));
 	});
 });
+
+describe('Undo manager', function() {
+	var doc = new m.Document(1,[new m.Section(2,[new m.P(4,["This is some text."])]), new m.Section(3,[])]);
+	var opsB = new Operations()
+		.retain(20)
+		.insert('This is some text')
+		.insert({_type:'P'})
+		.insert({_type:'Section'})
+		.insert('this is more text')
+		.insert({_type:'H3'})
+		.end(doc.length);
+
+	var other = new Operations()
+		.retain(17)
+		.insert(" that I want here.")
+		.end(doc.length);
+
+	var otherL = new Operations()
+		.retain(20)
+		.insert("This comes before. ")
+		.end(doc.length);
+
+	it('Can undo', function() {
+		var um = new UndoManager();
+		um.add(opsB);
+		var x = m.apply(doc, opsB);
+		var y;
+		um.performUndo(function(err, op) {
+			y = m.apply(x, op);
+			um.add(op);
+		})
+		assert.equal(JSON.stringify(y),JSON.stringify(doc));
+	});
+	it('Can redo', function() {
+		var um = new UndoManager();
+		um.add(opsB);
+		var x = m.apply(doc, opsB);
+		var y, z;
+		um.performUndo(function(err, op) {
+			y = m.apply(x, op);
+			um.add(op);
+		});
+		um.performRedo(function(err, op) {
+			z = m.apply(y, op);
+			um.add(op);
+		});
+		assert.equal(JSON.stringify(y),JSON.stringify(doc));
+		assert.equal(JSON.stringify(z),JSON.stringify(x));
+	});
+	it('Can undo after other op', function() {
+		var um = new UndoManager();
+		var justOther = m.apply(doc, other);
+		var otherP = other.transform(opsB);
+		//our apply
+		var x = m.apply(doc, opsB);
+		um.add(opsB);
+		//their apply
+		var both = m.apply(x, otherP);
+		um.transform(otherP);
+		var y;
+		um.performUndo(function(err, op) {
+			y = m.apply(both, op);
+			um.add(op);
+		});
+		assert.equal(JSON.stringify(justOther),JSON.stringify(y));
+	});
+	it('Can undo after other op with left', function() {
+		var um = new UndoManager();
+		var justOther = m.apply(doc, otherL);
+		var otherP = otherL.transform(opsB, true);
+		//our apply
+		var x = m.apply(doc, opsB);
+		um.add(opsB);
+		//their apply
+		var both = m.apply(x, otherP);
+		um.transform(otherP);
+		var y;
+		um.performUndo(function(err, op) {
+			y = m.apply(both, op);
+			um.add(op);
+		});
+		assert.equal(JSON.stringify(justOther),JSON.stringify(y));
+	});
+})
