dff4f3703e16108fa4d819dec1629bc4dea34899
Benjamin Norrington <ben@norrington.net>
2015-03-10 15:16:26 +0000

    extracted dummy websocket

---
+++ test/share.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+
+var slate0 = require('slate0');
+var sharejs = require('share').client;
+sharejs.registerType(slate0.type);
+
+var ottypes = {};
+ottypes[slate0.type.name] = ottypes[slate0.type.uri] = slate0.type;
+
+
+describe('ShareJS Integration', function() {
+});
