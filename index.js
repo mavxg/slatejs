13f2fceaddc5a2d976bcfbcf7945407d1ea4f9bc
Benjamin Norrington <ben@norrington.net>
2016-05-05 09:47:06 +0100

    support dummy socket

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var ids    = require('./ids');
 var editor = require('./editor');
+var ottype = require('ot-sexpr');
+
+var ottypes = {};
+ottypes[ottype.name] = ottypes[ottype.uri] = ottype;
 
 module.exports = {
 	Editor: editor.Editor,
 	Document: editor.Document,
-	type: require('ot-sexpr'),
+	type: ottype,
+	ottypes: ottypes,
 	render: require('./render'),
 	plugins: {
 		base: require('./plugins/base'),
